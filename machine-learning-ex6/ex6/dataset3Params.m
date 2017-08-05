a4c69e1f36517c0d1830410656054d65ce276af2
Robin Weston <robinweston@outlook.com>
2017-08-05 11:26:59 +0100

    Found optimum values for C and sigma

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -7,27 +7,29 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
+function [bestC, bestSigma] = findBestParams()
+    possibleValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+    [p,q] = meshgrid(possibleValues, possibleValues);
+    combinations = [p(:) q(:)];
+
+    for i=1:length(combinations)
+        testC = combinations(i,1);
+        testSigma = combinations(i,2);
+        model = svmTrain(X, y, testC, @(x1, x2) gaussianKernel(x1, x2, testSigma));
+        predictions = svmPredict(model,Xval);
+        err = mean(double(predictions ~= yval));
+        combinations(i,3) = err;
+    end
+
+    [_, minRowIndex] = min(combinations);
+    minRow = combinations(minRowIndex(3),:);
+    bestC = minRow(1);
+    bestSigma = minRow(2);
+end
+
+% [C, sigma] = findBestParams();
 C = 1;
-sigma = 0.3;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-
-
-
-
-
+sigma = 0.1;
 
 % =========================================================================
 
